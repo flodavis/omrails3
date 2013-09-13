718a20b16fe836a3114b3d149898e8186550a898
fpdavis86 <fpdavis86@gmail.com>
2013-09-12 17:32:37 -0700

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
 attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :provider, :uid, :as => [:default, :admin]
 
-has_many :pins
+has_many :pins, :dependent => :destroy
 
 validates_presence_of :name
 end
