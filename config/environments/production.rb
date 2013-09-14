aa1689bb01e87af91b11ae73b367a54caf0fb6ae
fpdavis86 <fpdavis86@gmail.com>
2013-09-13 19:59:50 -0700

    aws again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,12 @@ Omrails2::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => 'http://powerful-journey-1746.herokuapp.com/' }
   
-  
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
